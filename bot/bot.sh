1251cb20063c05d33f8cf63b7745d7bddae2bf5e
XYR <169094507+xyoruz@users.noreply.github.com>
2024-05-26 00:32:07 +0700

    Update bot.sh

--- bot/bot.sh
+++ bot/bot.sh
@@ -3,70 +3,55 @@ NS=$( cat /etc/xray/dns )
 PUB=$( cat /etc/slowdns/server.pub )
 domain=$(cat /etc/xray/domain)
 #color
+
+cd /etc/systemd/system/
+rm -rf kyt.service
+cd
 grenbo="\e[92;1m"
 NC='\e[0m'
 #install
-clear
-history -c 
-rm -fr xbot.sh
-rm -fr /usr/bin/kyt
-rm -fr /usr/bin/xdbot.zip*
-#color
-NC='\e[0m'
-u="\033[1;36m"
-y="\033[1;93m"
-g="\033[1;92m"
-r="\033[1;91m"
-
-REPO="https://raw.githubusercontent.com/sanzVPN/Azerd/ABSTRAK/bot/"
-NS=$( cat /etc/xray/dns )
-PUB=$( cat /etc/slowdns/server.pub )
-domain=$(cat /etc/xray/domain)
-#install
+cd /usr/bin
+rm -rf kyt
+rm -rf bot
 apt update && apt upgrade
-apt install neofetch -y
 apt install python3 python3-pip git
 cd /usr/bin
-wget -q -O bot.zip "${REPO}bot.zip"
+wget https://raw.githubusercontent.com/xyoruz/scriptvpn/main/Bot/bot.zip
 unzip bot.zip
 mv bot/* /usr/bin
 chmod +x /usr/bin/*
 rm -rf bot.zip
 clear
-wget -q -O xdbot.zip "${REPO}xbot.zip"
-unzip xdbot.zip
+cd
+wget https://raw.githubusercontent.com/xyoruz/scriptvpn/main/Bot/kyt.zip
+unzip kyt.zip
+cp -r kyt /usr/bin/
+cd /usr/bin
 pip3 install -r kyt/requirements.txt
-
-clear
+#isi data
 echo ""
-figlet 'SANZ' | lolcat
-echo -e "$u ┌────────────────────────────────────────────────┐${NC}"
-echo -e "$u │ \e[1;97;101m                ADD BOT PANEL                 ${NC} ${u}│${NC}"
-echo -e "$u └────────────────────────────────────────────────┘${NC}"
-echo -e "$u ┌────────────────────────────────────────────────┐${NC}"
-echo -e "$u │ ${g}Tutorial Creat Bot and ID Telegram                   ${NC}"
-echo -e "$u │ ${g}Creat Bot and Token Bot : @BotFather                 ${NC}"
-echo -e "$u │ ${g}Info Id Telegram : @MissRose_bot perintah /info      ${NC}"
-echo -e "$u └────────────────────────────────────────────────┘${NC}"
-echo -e ""
-read -e -p "  [*] Input your Bot Token : " bottoken
-read -e -p "  [*] Input Your Id Telegram : " admin
+figlet  Capmodz Vpn  | lolcat
+echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e " \e[1;97;101m          ADD BOT PANEL          \e[0m"
+echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "${grenbo}Tutorial Creat Bot and ID Telegram${NC}"
+echo -e "${grenbo}[*] Creat Bot and Token Bot : @BotFather${NC}"
+echo -e "${grenbo}[*] Info Id Telegram : @MissRose_bot , perintah /info${NC}"
+echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+read -e -p "[*] Input your Bot Token : " bottoken
+read -e -p "[*] Input Your Id Telegram :" admin
 echo -e BOT_TOKEN='"'$bottoken'"' >> /usr/bin/kyt/var.txt
 echo -e ADMIN='"'$admin'"' >> /usr/bin/kyt/var.txt
 echo -e DOMAIN='"'$domain'"' >> /usr/bin/kyt/var.txt
 echo -e PUB='"'$PUB'"' >> /usr/bin/kyt/var.txt
 echo -e HOST='"'$NS'"' >> /usr/bin/kyt/var.txt
+rm /etc/bot/.bot.db
+echo "#bot# ${bottoken} ${admin}" >>/etc/bot/.bot.db
 clear
 
-if [ -e /etc/systemd/system/xbot.service ]; then
-echo ""
-else
-rm -fr /etc/systemd/system/xbot.service
-fi
-
-cat > /etc/systemd/system/xbot.service << END
+cat > /etc/systemd/system/kyt.service << END
 [Unit]
-Description=Simple Bot Tele By - @xdxl_store
+Description=Simple kyt - @kyt
 After=network.target
 
 [Service]
@@ -78,33 +63,21 @@ Restart=always
 WantedBy=multi-user.target
 END
 
-systemctl daemon-reload
-systemctl start xbot
-systemctl enable xbot
-systemctl restart xbot
-cd
-
-# // STATUS SERVICE BOT
-bot_service=$(systemctl status xbot | grep active | awk '{print $3}' | cut -d "(" -f2 | cut -d ")" -f1)
-if [[ $bot_service == "running" ]]; then 
-   sts_bot="${g}[ON]${NC}"
-else
-   sts_bot="${r}[OFF]${NC}"
-fi
-
-rm -fr /usr/bin/bot.zip
-rm -fr /usr/bin/xdbot.zip
+systemctl start kyt 
+systemctl enable kyt
+systemctl restart kyt
+cd /root
+rm -rf kyt*
 clear
-neofetch
-echo -e "  ${y} Your Data BOT Info"
-echo -e "  ${u}┌───────────────────────────────────┐${NC}"
-echo -e "  ${u}│$r Status BOT ${y}=$NC $sts_bot "
-echo -e "  ${u}│$r Token BOT  ${y}=$NC $bottoken "
-echo -e "  ${u}│$r Admin ID   ${y}=$NC $admin "
-echo -e "  ${u}│$r Domain     ${y}=$NC $domain "
-#echo -e "  ${u}│$r NS Domain  ${y}=$NC $NS "
-echo -e "  ${u}└───────────────────────────────────┘${NC}"
-echo -e ""
-history -c
-read -p "  Press [ Enter ] to back on menu"
+echo "Done"
+echo "Your Data Bot"
+echo -e "==============================="
+echo "Token Bot         : $bottoken"
+echo "Admin          : $admin"
+echo "Domain        : $domain"
+echo -e "==============================="
+echo "Setting done"
+echo "Installations complete, type /menu on your bot"
+echo " "
+read -p "press any key for exit"
 menu
